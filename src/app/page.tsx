cff519c02f7981a8aa588d7580ef1831c6a7174d
dokugo90 <dokugo90@gmail.com>
2023-04-28 18:43:31 +0000

    Image Optimization NextJs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,116 +1,133 @@
+"use client"
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import React, { SetStateAction, createContext, useRef, useState } from 'react';
+import { DownloadOutlined, InboxOutlined, UploadOutlined } from '@ant-design/icons';
+import { Button, Space, } from 'antd';
+import axios from "axios"
+import pica from 'pica';
+import Compressor from 'compressorjs';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const [image, setImage] = useState(null);
+  const [fileData, setFileData] = useState<string | ArrayBuffer | null>(null)
+  const [fileName, setFileName] = useState("")
+  const [loading, setLoading] = useState(false);
+  const hiddenFileInput = useRef<any>(null);
+
+  function handleClick() {
+    hiddenFileInput!.current!.click()
+  }
+
+  function handleFileUpload(event: any) {
+    const MAX_SIZE = 5 * 1024 * 1024;
+    const file = event.target.files[0];
+    if (file.size > MAX_SIZE) {
+      alert("File size exceeds 5 megabytes")
+    return;
+  }
+
+  const extension = file.name.split(".")[1];
+  const imageExtensions = ['jpg', 'jpeg', 'png', 'gif', 'svg'];
+
+  if (imageExtensions.includes(extension.toLowerCase())) {
+    new Compressor(file, {
+      quality: 0.1,
+      maxHeight: 400,
+      maxWidth: 400,
+      success: (compressedResult) => {
+        // compressedResult has the compressed file.
+        // Use the compressed file to upload the images to your server.  
+        const reader = new FileReader();
+    reader.readAsDataURL(compressedResult);
+    reader.onloadend = () => {
+        setImage(reader.result as SetStateAction<null>);
+      setFileData(reader.result)
+      setFileName(file.name);
+      console.log(reader.result)
+  }      
+      },
+    });
+  } else {
+    alert("File is not a image")
+  }
+   
+}
+
+async function handleFileOptimization() {
+  try {
+    const extension = fileName.split(".")[1];
+    const imageExtensions = ['jpg', 'jpeg', 'png', 'gif', 'svg'];
+    if (imageExtensions.includes(extension.toLowerCase())) {
+      setLoading(true);
+      const response = await axios.post('https://image-optimization-api.vercel.app/optimize', { image: fileData })
+    .then((image) => {
+      console.log('Optimized image:', image.data);
+      setImage(image.data)
+      setLoading(false);
+    //alert(image.data);
+    }).catch((err) => {
+      alert("There was an error, please try again.")
+      setLoading(false);
+    });
+    } else {
+      alert("File is not a image")
+    }
+    
+  } catch (error) {
+    alert(error);
+  }
+}
+
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <main className='box-border w-screen h-screen p-0 bg-gray-950'>
+      <h1 className='p-2 text-4xl font-bold text-center text-blue-700'>Image Optimizer</h1>
+      <Button onClick={() => handleClick()} className='flex items-center justify-center m-auto mt-4 mb-4 bg-blue-500' type="primary" icon={<DownloadOutlined />} size={'large'}>
+            Upload Image
+          </Button>
+          <h2 className='text-base font-bold text-center text-white'>{fileName}</h2>
+      <input
+        type="file"
+        ref={hiddenFileInput}
+        onChange={handleFileUpload}
+        className='hidden'
+      />
+    <div className='flex justify-center'>
+      <div className='flex justify-center items-center w-[30%] min-w-[250px] max-w-[400px] h-[350px] bg-slate-200 rounded-3xl flex-col'>
+      {
+        loading ? 
+        <div className="custom-loader"></div>
+        :
+        image != null ?
+        <>
+        {image && <img className='w-[70%] h-[70%] object-contain rounded-3xl m-2' src={image} alt="Selected file" />}
+      <div>
+      <button onClick={() => handleFileOptimization()} className='px-4 py-2 mr-2 text-white transition duration-75 ease-in bg-blue-500 rounded cursor-pointer hover:shadow-xl'>
+        Optimize
+      </button>
+      <button onClick={() => {
+        setImage(null)
+        setFileData(null)
+        setFileName("")
+      }} className='px-4 py-2 text-white transition duration-75 ease-in bg-red-500 rounded cursor-pointer hover:shadow-xl'>
+        Delete
+      </button>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      </>
+      : 
+      <>
+      <div className='flex justify-center items-center w-[100%] h-[100%] flex-col'>
+        <InboxOutlined className='text-blue-500 text-7xl' />
+        <h2 >Choose a image file to optimize</h2>
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      </>
+      }
+    </div>
+    </div>
     </main>
   )
 }
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,34 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        'primary': "black"
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      screens: {
+        'sm': "640px",
+        'md': '768px',
+        'lg': "1024px",
+        'xl': '1280px'
+      },
+      animation: {
+        pop: 'pop 1s ease-in alternate infinite'
+      },
+      keyframes: {
+          pop: {
+            '0%': {
+              transform: 'scale(0)'
+            },
+            '100%': {
+              transform: 'scale(1)'
+            }
+          },
+         
+      }
     },
   },
   plugins: [],
